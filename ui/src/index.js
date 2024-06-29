106bd44ac12043a6697f67825bc03102f606fdae
nuwansamaware <nuwansam@awarelabs.io>
2024-06-29 12:03:21 +1000

    add notel env

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,18 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { TestChimpSDK } from "testchimp-js";
 
+const TC_PROJECT_ID=process.env.TESTCHIMP_PROJECT_ID;
+const TC_SESSION_RECORD_KEY=process.env.TESTCHIMP_SESSION_RECORD_API_KEY;
+const TESTCHIMP_TRACED_URI_REGEX=process.env.TESTCHIMP_TRACED_URI_REGEX;
+const TESTCHIMP_UNTRACED_URI_REGEX=process.env.TESTCHIMP_UNTRACED_URI_REGEX
+
 window.onload = function () {
   TestChimpSDK.startRecording({
     enableRecording: true,
-    projectId: "de40f23a-61db-40b1-91ee-2e42435c0fab",
-    sessionRecordingApiKey: "3ac2f316-95a0-441b-9132-aba1a4e24bf6",
+    projectId: TC_PROJECT_ID,
+    sessionRecordingApiKey: TC_SESSION_RECORD_KEY,
     endpoint: "https://ingress-staging.testchimp.io",
     samplingProbabilityOnError: 0.1,
     samplingProbability: 1.0,
     maxSessionDurationSecs: 500,
     eventWindowToSaveOnError: 200,
-    tracedUriRegexListToTrack: ".*://shoplify-staging-fe.testchimp\.io.*$",
-    untracedUriRegexListToTrack: "/^$/",
+    tracedUriRegexListToTrack: TESTCHIMP_TRACED_URI_REGEX,
+    untracedUriRegexListToTrack: TESTCHIMP_UNTRACED_URI_REGEX,
     environment: "QA"
   });
 };
